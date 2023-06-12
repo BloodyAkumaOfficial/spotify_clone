f14475681c38fc1922d92d4808be4c721340ecfc
denys <denys.khrushchov@icloud.com>
2023-06-12 17:32:39 +0200

    create schemas and connet to mongo db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TrackModule } from "./track/track.module";
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [TrackModule]
+    imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:root@cluster0.rkwizk8.mongodb.net/?retryWrites=true&w=majority'),
+        TrackModule
+    ]
 })
 export class AppModule {}
